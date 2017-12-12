20e69b2048f48c2f1ac51543aa9e869726d02e55
tommyso <31903800+tommyso@users.noreply.github.com>
2017-12-12 04:46:56 -0500

    Add files via upload
    Expresion Regular de palabras

---
+++ Expresion_Regular_Tarea.py
@@ -0,0 +1,13 @@
+import re
+
+if __name__ == '__main__':
+
+    #patron = '[a-zA-Z0-9|\W]ap[\W|[a-oq-z0-9]'
+    patron = '[a-ce-gi-oq-z0-9][a-z0-9][{a-df-qs-z}\s][{a-ce-z}\s]'
+    palabras = ['rap them', 'tapeth', 'apth', 'wrap/try', 'sap tray', '87ap9th', 'apothecary', 'aleht', 'happy them', 'tarpth', 'Apt', 'peth', 'tarreth', 'ddpdg']
+
+    for palabra in palabras:
+        if re.match(patron, palabra):
+            print("La palabra " + palabra + " cumple.")
+        else:
+            print("La palabra " + palabra + " no cumple.")
